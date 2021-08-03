f48f6969b4cc31408d09525ed2376ffc25117bc7
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 14:39:08 +0300

    Add files via upload

---
+++ Sets/Sets/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+            for (int i = 0; i < n ; i++)
+            {
+                string curName = Console.ReadLine();
+                names.Add(curName);
+            }
+            Console.WriteLine(String.Join('\n',names));
+        }
+    }
+}
